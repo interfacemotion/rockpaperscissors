28b2f9b9a33319448ebdd0a09e7a0a1ae0d66339
interfacemotion <interfacemotion@proton.me>
2024-03-09 10:15:46 +0100

    Code refactored. Fully working. Leaving as is for now.

--- script.js
+++ script.js
@@ -21,15 +21,6 @@ function getComputerChoice () {
             break;
     }
     console.log(`The computers's choice is ${choice}.`);
-    // if (randnum == 3) {
-    //     choice = "rock";
-    // } else if (randnum == 2) {
-    //     choice = "scissors";
-    // } else {
-    //     choice = "paper";
-    // }
-//    return;
-//    console.log("");
 }
 
 function gameCounter () {
@@ -65,83 +56,73 @@ function checkPassedInput () {
     }
 }
 
-function playRockGame () {
-    console.log(`Your choice is ${playerChoice}.`);
-    getComputerChoice();
-//    ++gamesPlayed;
-//    console.log(playerChoice);
-    switch (playerChoiceUpper) {
-        case "ROCK":
-            switch (choice) {
-                case "scissors": 
-                    console.log("Rock beats scissors. Human Wins!");
-                    ++human;
-                    gameCounter();
-                    break;
-                case "paper":
-                    console.log("Paper beats rock. Machine Wins!");
-                    ++machine;
-                    gameCounter();
-                    break;
-                case "rock":
-                    console.log("It's a tie!");
-                    gameCounter();
-                    break;
-            }
-            break;
-        case "PAPER":
-            switch (choice) {
-                case "scissors": 
-                    console.log("Scisssors beats paper. Machine Wins!");
-                    ++machine;
-                    gameCounter();
-                    break;
-                case "paper":
-                    console.log("It's a tie!");
-                    gameCounter();
-                    break;
-                case "rock":
-                    console.log("Paper beats rock. Human Wins!");
-                    ++human;
-                    gameCounter();
-                    break;
-            }
-            break;
-        case "SCISSORS":
-            switch (choice) {
-                case "scissors": 
-                    console.log("It's a tie!");
-                    gameCounter();
-                    break;
-                case "paper":
-                    console.log("Scissors beats paper. Human Wins!");
-                    ++human;
-                    gameCounter();
-                    break;
-                case "rock":
-                    console.log("Rock beats scissors. Machine Wins!");
-                    ++machine;
-                    gameCounter();
-                    break;
-            }
-            break;
-        default:
-            console.log("Invalid input has been given.");
-    }
-}
-
 function playGame () {
     if (initialize) {
         checkPassedInput();
     } else {
         for (let gamesPlayed = 0; gamesPlayed < gameNumber; gamesPlayed++) {
-                playRockGame();
+            console.log(`Your choice is ${playerChoice}.`);
+            getComputerChoice();
+            switch (playerChoiceUpper) {
+                case "ROCK":
+                    switch (choice) {
+                        case "scissors": 
+                            console.log("Rock beats scissors. Human Wins!");
+                            ++human;
+                            gameCounter();
+                            break;
+                        case "paper":
+                            console.log("Paper beats rock. Machine Wins!");
+                            ++machine;
+                            gameCounter();
+                            break;
+                        case "rock":
+                            console.log("It's a tie!");
+                            gameCounter();
+                            break;
+                    }
+                    break;
+                case "PAPER":
+                    switch (choice) {
+                        case "scissors": 
+                            console.log("Scisssors beats paper. Machine Wins!");
+                            ++machine;
+                            gameCounter();
+                            break;
+                        case "paper":
+                            console.log("It's a tie!");
+                            gameCounter();
+                            break;
+                        case "rock":
+                            console.log("Paper beats rock. Human Wins!");
+                            ++human;
+                            gameCounter();
+                            break;
+                    }
+                    break;
+                case "SCISSORS":
+                    switch (choice) {
+                        case "scissors": 
+                            console.log("It's a tie!");
+                            gameCounter();
+                            break;
+                        case "paper":
+                            console.log("Scissors beats paper. Human Wins!");
+                            ++human;
+                            gameCounter();
+                            break;
+                        case "rock":
+                            console.log("Rock beats scissors. Machine Wins!");
+                            ++machine;
+                            gameCounter();
+                            break;
+                    }
+                    break;
+                default:
+                    console.log("Invalid input has been given.");
+            }
         }
     }
 }
 
 playGame();
-
-// console.log(playerChoice)
-
-// console.log(choice);
